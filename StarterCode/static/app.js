9c352de4d2f76ac18da7ba52dd85bd1df30ae9f0
nataliesmith32 <natalielsmith32@gmail.com>
2021-02-22 10:36:28 -0700

    finished bubble

--- StarterCode/static/app.js
+++ StarterCode/static/app.js
@@ -12,7 +12,6 @@ d3.json('samples.json').then((data)=>{
 // make the plot with selected test id and pull in data
 function makePlot(testId){
     d3.json('samples.json').then((data)=>{
-        // This is the array
         var samples=data.samples;
         var testNum=samples.map(row=>row.id).indexOf(testId);
         // Make bar plot
@@ -20,4 +19,49 @@ function makePlot(testId){
         var otuValueTen=otuValueTen[testNum].slice(0,10).reverse();
         var otuIdTen=samples.map(row=>row.otu_ids);
         var otuIdTen=otuIdTen[testNum].slice(0,10);
+        var otuLabelTen=samples.map(row=>row.otu_labels); 
+        var otuLabelTen=otuLabelTen[testNum].slice(0,10); 
+        var trace={
+            x: otuValueTen,
+            y: otuIdTen.map(r=>`UTO ${r}`),
+            text: otuLabelTen,
+            type:'bar',
+            orientation:'h'
+        }
+        Plotly.newPlot('bar',[trace]);
+        // make bubble chart
+        var otuValue=samples.map(row=>row.sample_values);
+        var otuValue=otuValue[testNum];
+        var otuId=samples.map(row=>row.otu_ids);
+        var otuId=otuId[testNum];
+        var otuLabel=samples.map(row=>row.otu_labels); 
+        var otuLabel=otuLabel[testNum];
+        var minIds=d3.min(otuId);
+        var maxIds=d3.max(otuId);
+        var mapNr = d3.scaleLinear().domain([minIds, maxIds]).range([0, 1]);
+        var bubbleColors = otuId.map( val => d3.interpolateRgbBasis(["royalblue", "greenyellow", "goldenrod"])(mapNr(val)));
+        var trace1={
+            x: otuId,
+            y: otuValue,
+            text: otuLabel,
+            mode: 'markers',
+            marker: {
+                color: bubbleColors,
+                size: otuValue.map(x=>x*10),
+                sizemode: 'area'
+            }
+        };
+        var data1=[trace1];
+        var bubbleLayout={
+            xaxis:{
+                autochange: true,
+                height: 600,
+                width: 1000,
+                title: {
+                    text: 'OTU ID'
+                }
+            },
+        };
+        Plotly.newPlot('bubble',data1,bubbleLayout);   
+   
       
